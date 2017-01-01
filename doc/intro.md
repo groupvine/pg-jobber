d86653e95dcc5751bc840b22872fd8a05369aede
Dave Beyer <dave.beyer@gmail.com>
2016-12-31 20:04:08 -0800

    add jsdoc comments; add build using jsdoc2md to produce README.md that includes the API

---
+++ doc/intro.md
@@ -0,0 +1,68 @@
+# Postres-based Job Manager (pg-jobber)
+
+A simple, responsive job queue manager based on PostgreSQL 9.6+,
+designed for clusters of up to about 5-10 workers per job type.
+
+* Simple, promise-based API for job requesters and handler interface
+  for workers.
+
+* Flexible, JSON-based job request instructions and results.
+
+* Takes advantage of "SKIP LOCKED" and "LISTEN/NOTIFY" features of
+  Postgres 9.6 for robust and responsive performance.
+
+* Supports an arbitrary number of job types and worker pools,  differing 
+  job priorities, and tracking of job processing performance.
+
+**Note that this module will create a "pgjobber_jobs" table in the associated 
+PostgreSQL database, if not already present.**
+
+## Example Usage
+
+### Initialize jobber
+
+To be done once during server startup initialization 
+(same for job requesters and workers).
+
+```
+var myId     = "server-10.0.0.17";
+var pgconfig = {host : '10.0.0.11', port : 5432, database : 'mydb', 
+                user : 'postgres', password : 'password'};
+
+var jobber   = require('pg-jobber')(myId, pgconfig);
+```
+
+
+### Requester
+
+Issuing a job request:
+
+```
+jobber.request("calculator", [5, '+', [2, '*', 3]]).then(instr, results => {
+    console.info(`5 + (2 x 3) = ${results}`);
+}).catch(err) {
+    console.error("Calculator job request failed with error:", err);
+});
+```
+
+### Worker
+
+Registering for a job type and processing jobs:
+
+```
+jobber.handle('calculator', instr => {
+
+    function calculate(num1:any, op:string, num2:any) {
+        if (Array.isArray(num1)) { num1 = calculate(num1); }
+        if (Array.isArray(num2)) { num2 = calculate(num2); }
+
+        if (typeof num1 !== 'number' || typeof num2 !== 'number' || !op.match(/[\+\-\*\/]/)) {
+            throw `Invalid operation: ${num1} ${op} ${num2}`
+        }
+
+        return eval(num1 + op + num2);
+    }
+
+    return calculate(instr);
+});
+```
--- index.ts
+++ index.ts
@@ -9,12 +9,43 @@ class Jobber {
     serverId : string;
     pgConfig : any;
     options  : any;
+    
+    /**
+     * A Postgres-based job scheduling utility 
+     * for relatively small server clusters.
+     *
+     * Constructor, optionally invoked with serverId and 
+     * Postgres configuration data (otherwise, must call .init() 
+     * with this info).
+     * 
+     * @constructor Jobber
+     * @param {string=} serverId
+     * @param {Object=} pgConfig
+     * @param {Object=} options
+     */
 
-    constructor (serverId?:any, pgConfig?:any, options?:any) {
+    constructor (serverId?:string, pgConfig?:any, options?:any) {
         if (serverId && pgConfig) {
             this.init(serverId, pgConfig, options);
         }
     }
+
+    /**
+     * Initialize jobber (if not already done so in construction).
+     *
+     * @method Jobber#init
+     *
+     * @param {string} serverId - Unique string identifying this server
+     *
+     * @param {Object} pgConfig - Postgres configuration, must include
+     *     properties: host {string}, port {number}, database {string},
+     *     user {string}, and password {string}.
+     *
+     * @param {Object=} options  - Optional configuration info, with 
+     *     properties: logger {Bunyan compatible logger}
+     *
+     * @returns {void}
+     */
 
     public init(serverId:string, pgConfig:any, options?:any) {
         this.serverId = serverId;
@@ -44,6 +75,52 @@ class Jobber {
             });
         });
     }
+
+    /**
+     * Request a new job
+     *
+     * @method Jobber#request
+     * 
+     * @param {string} jobType - String identifying the job type and associated worker pool
+     * @param {Object} instr   - Job-specific instructions
+     *
+     * @returns {Promise.<Object,Object>} A promise that's resolved to the job 
+     *     instructions and results objects.
+     */
+
+    public request(jobType:string, instr:any) {
+        // Issue a new job request
+        return new Promise( (resolve, reject) => {
+            return Promise.resolve(5);
+        });
+    }
+
+    /**
+     * Handler callback
+     *
+     * @callback Jobber#handle#handlerCB(instr)
+     *
+     * @param   {Object} instr - Requested job instructions
+     * @returns {Object} Job results
+     */
+
+    /**
+     * Register a handler for a particular job type
+     *
+     * @method Jobber#handle
+     * 
+     * @param {string} jobType - String identifying the job type to be handled
+     * @param {handlerCB} handlerCb
+     *
+     * @returns {void}
+     */
+    public handle(jobType:string, handlerCb:any) : void {
+        // Register this server as a handler for this jobType
+    }
+
+    //
+    // Private helper methods
+    //
 
     private initDB(db:any) {
         let _this = this;
